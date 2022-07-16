5ea4942bb18f0d30fed5cd637194bda960176ebf
Robert Lucey <robert@glimpse.ie>
2022-07-16 16:59:41 +0100

    Test get_para_hashes

--- test/learn/test_base_tagged_docs.py
+++ test/learn/test_base_tagged_docs.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
 from mock import patch
 
+from oireachtas_data.models.para import Para
+
 from oireachtas_nlp.models.text import TextBody
 from oireachtas_nlp.learn.base_tagged_docs import BaseTaggedDocs
 
@@ -35,3 +37,29 @@ class BaseTaggedDocsTest(TestCase):
         self.assertEqual(len(res), 2)
         self.assertEqual(res[0].tags, ['speaker_1_0'])
         self.assertEqual(res[0].words, ['content_1'])
+
+    @patch(
+        'oireachtas_nlp.learn.base_tagged_docs.BaseTaggedDocs.should_include',
+        side_effect=lambda x: True
+    )
+    def test_get_para_hashes(self, mock_should_include):
+        docs = BaseTaggedDocs()
+
+        para = Para(
+            title='title 1',
+            eid=None,
+            content='This is the content of title 1'
+        )
+        para_2 = Para(
+            title='title 1',
+            eid=None,
+            content='This is the content of title 2'
+        )
+
+        docs.load('speaker_1', [para])
+        docs.load('speaker_2', [para_2])
+
+        self.assertEqual(
+            docs.get_para_hashes(),
+            {311784746898390805096967306140810741725, 223673265617601130829714390136355227675}
+        )
