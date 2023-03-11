013dfd263936ebccc51ef661099acd7822a9e2d1
Robert Lucey <robert@glimpse.ie>
2023-03-11 14:11:14 +0000

    Bump 1.0.14

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ INSTALL_REQUIRES = (
 
 setup(
     name='oireachtas-nlp',
-    version='1.0.13',
+    version='1.0.14',
     python_requires='>=3.6',
     description='nlp on data from the oireachtas api relating to members / parties',
     long_description='nlp on data from the oireachtas api relating to members / parties',
